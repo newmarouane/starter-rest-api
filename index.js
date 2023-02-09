0be2bdb22755cfdfe2aac0a46f8f843b4daa3b94
newmarouane <80288727+newmarouane@users.noreply.github.com>
2023-02-09 09:48:41 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,120 +1,54 @@
-const express = require('express')
-const app = express()
+// ./src/index.js
+// importing the dependencies
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
 
-const fs = require('fs');
+const fetch = require('node-fetch');
 const puppeteer = require('puppeteer');
+// defining the Express app
+const app = express();
+
+// adding Helmet to enhance your Rest API's security
+//app.use(helmet());
+
+// using bodyParser to parse JSON bodies into JS objects
+app.use(bodyParser.json());
+
+// enabling CORS for all requests
+//app.use(cors());
+
+// adding morgan to log HTTP requests
+app.use(morgan('combined'));
+
+// defining an endpoint to return all ads
+app.get('/', async(req, res)  => {
+  	const browser = await puppeteer.launch({
+  args: ['--no-sandbox', '--disable-setuid-sandbox'],
+});
+	const page = await browser.newPage();
+	await page.goto("https://jobviewtrack.com/fr-fr/job-1a13416048160c06461a440a1b041b15676206010a1c464a5a482f371fad8e17490204081da4cc21415f/abd846d5ff8abce585b15251956ee068.html?affid=0afaf0173305e4b8");
+	const jobDescription = await page.$eval('.content', el => el.innerHTML).catch((e)=> {
+		return "";
+	});	
+	const logo = await page.$eval('.container > header > img', el => el.getAttribute('src')).catch((e)=> {
+		return "";
+	});
+	var applyUrl = await page.$eval('.btn-apply', el => el.getAttribute('href')).catch((e)=> {
+		return "";
+	});
+	
+	
+	await browser.close();
+
+	res.send({"jobDescription":jobDescription,"logo":logo});
+}
+);
 
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-
-// #############################################################################
-// This configures static hosting for files in /public that have the extensions
-// listed in the array.
-// var options = {
-//   dotfiles: 'ignore',
-//   etag: false,
-//   extensions: ['htm', 'html','css','js','ico','jpg','jpeg','png','svg'],
-//   index: ['index.html'],
-//   maxAge: '1m',
-//   redirect: false
-// }
-// app.use(express.static('public', options))
-// #############################################################################
-
-
-// Get jobs list
-app.get('/api/jobs', async (req, res) => {
-  console.log(`fetch jobs list`)
-  let rawdata = fs.readFileSync('api.json');
-  let jobs = JSON.parse(rawdata);
-  
- 
-  
-   res.json(jobs).end()
-})
-
-
-app.get('/api/puppeteer', async (req, res) => {
-  console.log(`fetch website title`)
-  res.json({"html":"ok"}).end();
-  const browser = await puppeteer.launch()
-  const page = await browser.newPage()
-  await page.setDefaultNavigationTimeout(0);
-  //var url = 'https://jobviewtrack.com/fr-fr/job-194a416e4209020b5517490204413da4cc4411061d124359582f6e0b02034206430a090d6c34a689100d07074b424f482c5256581346/82e1ef6fc3c8e10766317158c66fd2c3.html';
-  var url = "https://www.optioncarriere.com/jobad/fr82e1ef6fc3c8e10766317158c66fd2c3";
-  //var url = "http://public.api.careerjet.net/search?locale_code=fr_fr&pagesize=2&user_ip=101.1.1.1&user_agent=a";
-  await page.goto(url);
-  
-  const inner_html = await page.$eval('.container', element => {
-    console.log('element.innerHTML: ' + element.innerHTML);
-    return element.innerHTML;
-  });
-  console.log('inner_html: ' + inner_html);
-
-   
-
-
-  
-  // Dispose of handle
-  await element.dispose();
-
-  // Close browser.
-  await browser.close();
-  
- 
-  
-})
-
-// Create or Update an item
-app.post('/:col/:key', async (req, res) => {
-  console.log(req.body)
-
-  const col = req.params.col
-  const key = req.params.key
-  console.log(`from collection: ${col} delete key: ${key} with params ${JSON.stringify(req.params)}`)
-  const item = await db.collection(col).set(key, req.body)
-  console.log(JSON.stringify(item, null, 2))
-  res.json(item).end()
-})
-
-// Delete an item
-app.delete('/:col/:key', async (req, res) => {
-  const col = req.params.col
-  const key = req.params.key
-  console.log(`from collection: ${col} delete key: ${key} with params ${JSON.stringify(req.params)}`)
-  const item = await db.collection(col).delete(key)
-  console.log(JSON.stringify(item, null, 2))
-  res.json(item).end()
-})
-
-// Get a single item
-app.get('/:col/:key', async (req, res) => {
-  const col = req.params.col
-  const key = req.params.key
-  console.log(`from collection: ${col} get key: ${key} with params ${JSON.stringify(req.params)}`)
-  const item = await db.collection(col).get(key)
-  console.log(JSON.stringify(item, null, 2))
-  res.json(item).end()
-})
-
-// Get a full listing
-app.get('/:col', async (req, res) => {
-  const col = req.params.col
-  console.log(`list collection: ${col} with params: ${JSON.stringify(req.params)}`)
-  const items = await db.collection(col).list()
-  console.log(JSON.stringify(items, null, 2))
-  res.json(items).end()
-})
-
-
-// Catch all handler for all other request.
-app.use('*', (req, res) => {
-  res.json({ msg: 'no route handler found' }).end()
-})
-
-// Start the server
-const port = process.env.PORT || 3000
-app.listen(port, () => {
-  console.log(`index.js listening on ${port}`)
-})
+// starting the server
+app.listen(3000, async() => {
+  console.log('Listening on port 3000');
+});
