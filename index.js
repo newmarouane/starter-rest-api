814792d2998ba3ea6ffffe69299fac47ce8b458c
newmarouane <80288727+newmarouane@users.noreply.github.com>
2023-02-16 02:08:12 +0100

    Update index.js

--- index.js
+++ index.js
@@ -61,6 +61,60 @@ app.get('/:col', async (req, res) => {
   res.json(items).end()
 })
 
+app.get('/job', async (req, res) => {
+try {
+  let browser;
+
+    browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox','--disable-gpu',
+        '--disable-dev-shm-usage',
+        '--no-first-run',
+        '--no-zygote',
+        '--deterministic-fetch',
+        '--disable-features=IsolateOrigins',
+        '--disable-site-isolation-trials',]} );
+  const page = await browser.newPage();
+  
+  var url = 'https://jobviewtrack.com/fr-fr/job-1a13416048160c06461a440a1b041b15676206010a1c464a5a482f371fad8e17490204081da4cc21415f/abd846d5ff8abce585b15251956ee068.html?affid=0afaf0173305e4b8';
+  console.log(url);
+  
+  await page.goto(url, {waitUntil: 'load', timeout: 90000});
+    
+	var jobDescription = await page.$eval('.content', el => el.innerHTML).catch((e)=> {
+		return "";
+	});
+    
+	const logo = await page.$eval('.container > header > img', el => el.getAttribute('src')).catch((e)=> {
+		return "";
+	});
+  
+	var applyUrl = await page.$eval('.btn-apply', el => el.getAttribute('href')).catch((e)=> {
+		return "";
+	});
+	if(applyUrl != null && applyUrl.startsWith("/")) applyUrl = await stringToOriginUrl(page.url()) + applyUrl.replace("/job/register/","/job/");
+    
+  var jobContract = await page.$eval('#job > div > header > ul.details > li:has([*|href*="contract"])', el => el.textContent.replace(/\n/g, "").replaceAll("  ", "")).catch((e)=> {
+		return "";
+	});
+	
+  var jobDuration = await page.$eval('#job > div > header > ul.details > li:has([*|href*="duration"])', el => el.textContent.replace(/\n/g, "").replaceAll("  ", "")).catch((e)=> {
+		return "";
+	});
+  
+  page.close();
+  browser.close();
+  var items= {"jobDescription":jobDescription,"logo":logo,"applyUrl":applyUrl, "jobContract": jobContract, "jobDuration": jobDuration}
+  res.json(items).end();
+}
+  catch(e) {console.log(
+            JSON.stringify({
+                error : true,
+                errorMessage : e.message
+            })
+        );
+    } finally {
+       // await browser.close();
+    } 
+}
 // Catch all handler for all other request.
 app.use('*', async (req, res) => {
    let animals = db.collection('animals')
