7b9a28cb9497be47c513243bbc18854faa569afa
newmarouane <80288727+newmarouane@users.noreply.github.com>
2023-02-16 02:03:37 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const db = require('@cyclic.sh/dynamodb')
+const puppeteer = require('puppeteer')
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
