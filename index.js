98f12728fbcec7f023d43abbc58a281f2e15a14a
newmarouane <80288727+newmarouane@users.noreply.github.com>
2023-02-16 11:26:07 +0100

    Update index.js

--- index.js
+++ index.js
@@ -124,7 +124,7 @@ try {
 
 app.use('/cron', async (req, res) => {
 	var url="https://confused-talented-poet.glitch.me/";
-	print(cron.validate('*/10 * 0 * * *'));
+	print(cron.validate('*/10 0 * * *'));
 	  cron.schedule('*/10 0 * * *', async () => {
 	    console.log("cron executed");
 	    const response = await fetch(url);
